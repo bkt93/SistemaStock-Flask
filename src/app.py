27bf91dfdd0616aa939933df7d1594e8de821d2b
Florencia Shapasnikoff <111530159+florsha35@users.noreply.github.com>
2023-09-08 17:13:49 -0300

    Creacion de ruta update
    creamos la ruta de conexion para modificar elementos de la base de datos mediante un update

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -15,10 +15,18 @@ mysql= MySQL(app)
 
 
 
-
+# Ruta principal
 @app.route('/')
 def Index():
-    return render_template('index.html')
+
+
+    # Consulta SELECT para acceder a todos los datos de la db
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM inventario")
+    inventario_items = cur.fetchall()
+    print(inventario_items)
+
+    return render_template('index.html', inv = inventario_items)
 
     
 # Ruta para renderizar el template para añadir nuevos elementos
@@ -42,17 +50,50 @@ def add_insumo():
         cur = mysql.connection.cursor()
         cur.execute('INSERT INTO inventario (insumos, registro, ubicacion, estado, precio_unitario, fecha_de_ingreso, fecha_de_actualizacion, observacion) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)', (insumos, registro, ubicacion, estado, precio_unitario, fecha_de_ingreso, fecha_de_actualizacion, observacion))
         mysql.connection.commit()
-        return "Recibido"
+        return redirect('/')
 
-# Ruta para editar elementos
-@app.route('/edit_insumo')
-def edit_insumo():
-    return render_template('edit.html')
+# Ruta para recuperar la información disponible en la DB para poder editar un elemento
+@app.route('/edit_insumo/<int:id>')
+def edit_insumo(id):
+
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM inventario WHERE id = %s', (id,))
+    inventario_items = cur.fetchall()
+    mysql.connection.commit()
+    return render_template('edit.html', inv = inventario_items)
+
+
+@app.route('/update/<id>', methods = ['POST'])
+def update(id):
+    if request.method == 'POST':
+        insumos = request.form['insumos']
+        registro = request.form['registro']
+        ubicacion = request.form['ubicacion']
+        estado = request.form['estado']
+        precio_unitario = request.form['precio_unitario']
+        fecha_de_ingreso = request.form['fecha_de_ingreso']
+        fecha_de_actualizacion = request.form['fecha_de_actualizacion']
+        observacion = request.form['observacion']
+        
+        cur = mysql.connection.cursor()
+        cur.execute("""
+                    UPDATE inventario
+                    SET insumos = %s, registro = %s, ubicacion  = %s, estado = %s, 
+                        precio_unitario = %s, fecha_de_ingreso = %s, fecha_de_actualizacion = %s,
+                        observacion = %s WHERE id = %s
+                    """, (insumos, registro, ubicacion, estado, precio_unitario, fecha_de_ingreso,
+                        fecha_de_actualizacion, observacion, id))
+        mysql.connection.commit()
+        return redirect('/')
 
 # Ruta para eliminar elementos
-@app.route('/delete_insumo')
-def delete_insumo():
-    return "Elemento eliminado"
+@app.route('/delete_insumo/<int:id>')
+def delete_insumo(id):
+      
+    cur = mysql.connection.cursor()
+    cur.execute('DELETE FROM inventario WHERE id = %s', (id,))
+    mysql.connection.commit()        
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
