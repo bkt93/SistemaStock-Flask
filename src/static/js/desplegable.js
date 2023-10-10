bc15ad6f4f9c476ed1b585e200d38bbf9d56c669
Gabriel <valdesgabriel1993@gmail.com>
2023-10-10 20:01:45 -0300

    Sidebard estilos

--- src/static/js/desplegable.js
+++ src/static/js/desplegable.js
@@ -1,8 +1,20 @@
 //Funcionalidades barra desplegable
 const cloud = document.getElementById("cloud");
-const barraLateral = document.querySelector(".barra-lateral")
+const barraLateral = document.querySelector(".barra-lateral");
+const spans = document.querySelectorAll("span");
+const palanca = document.querySelector(".switch");
+const circulo = document.querySelector(".circulo");
+
+palanca.addEventListener("click",()=>{
+    let body = document.body;
+    body.classList.toggle("dark-mode");
+    circulo.classList.toggle("prendido");
+});
 
 cloud.addEventListener("click", ()=>{
     barraLateral.classList.toggle("mini-barra-lateral");
-})
+    spans.forEach((span)=>{
+        span.classList.toggle("oculto");
+    });
+});
   
